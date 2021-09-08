66402401292c7f11d2585eaf236cee5fa85a17c2
Krisztian_Csepi <krisztian_csepi@epam.com>
2021-09-08 13:41:11 +0200

    adding tests to the model package #4

--- src/test/java/org/openvasp/client/model/JuridicalPersonIdTests.java
+++ src/test/java/org/openvasp/client/model/JuridicalPersonIdTests.java
@@ -9,10 +9,16 @@ import java.util.NoSuchElementException;
 public class JuridicalPersonIdTests {
 
     @Test
-    public void missingIdInformationTest() {
+    public void validationTest() {
         JuridicalPersonId juridicalPersonId = new JuridicalPersonId();
         VaspMessage vaspMessage = new SessionRequest();
         Assertions.assertThrows(VaspValidationException.class, () -> juridicalPersonId.validate(vaspMessage));
+
+        juridicalPersonId.setJurIdCountry(Country.fromCode("CH"));
+        juridicalPersonId.setJurId("123456AH");
+        juridicalPersonId.setJurIdIssuer("ID");
+        juridicalPersonId.validate(vaspMessage);
+        Assertions.assertDoesNotThrow(() -> juridicalPersonId.validate(vaspMessage));
     }
 
     @Test
